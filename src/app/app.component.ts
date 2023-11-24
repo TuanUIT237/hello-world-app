09c46ada6afe93ad08bce0084fcee9fbd5524809
TuanUIT237 <23072002td@gmail.com>
2023-11-24 12:37:49 +0700

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { UserService } from './user.service';
 
 @Component({
@@ -10,7 +10,7 @@ import { UserService } from './user.service';
   imports: [
     CommonModule, 
     RouterOutlet,
-    HttpClient
+    HttpClientModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
